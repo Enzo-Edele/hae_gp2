61fe7f0b33c682720cfc05b6ebd918588c97633c
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2022-11-15 17:07:00 +0100

    du sang et des larmes

--- Cour4 10-11/ConsoleAppCour4.cpp
+++ Cour4 10-11/ConsoleAppCour4.cpp
@@ -1,5 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
+#include<SFML/Window.hpp>
+#include<filesystem>
+
+#include "CatMull.hpp"
+#include "Line.hpp"
 
 using namespace std;
 
@@ -8,10 +14,10 @@ void TestTuto() {
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
+    while (window.isOpen()) //une frame
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window.pollEvent(event)) //un event  il y a d'autre mode d'interraction que poll
         {
             if (event.type == sf::Event::Closed)
                 window.close();
@@ -38,6 +44,7 @@ void TestLine() {
     convex.setPoint(3, sf::Vector2f(100, 120));
     convex.setPoint(4, sf::Vector2f(70, 120));
     convex.setFillColor(sf::Color::Red);
+    convex.setOrigin(300, 500);
 
     sf::RectangleShape rect(sf::Vector2f(150, 100));
     rect.setFillColor(sf::Color::Black);
@@ -89,8 +96,130 @@ void TestLine() {
     int here =  0;
 }
 
+static int gameSizeX = 1080;
+static int gameSizeY = 720;
+static int groundH = 620;
+
+static float catmullT = 0.5f;
+
+static sf::RectangleShape ground(sf::Vector2f(gameSizeX, 2));
+sf::RectangleShape canon(sf::Vector2f(20, 100));
+static std::vector<sf::RectangleShape> catmullPoints;
+static sf::VertexArray sight(sf::PrimitiveType::Lines);
+
+static int cannonRotation = -45;
+
+void TestVertex() {
+    sf::ContextSettings settings(0, 0, 2);
+    sf::RenderWindow window(sf::VideoMode(gameSizeX, gameSizeY), "Vertex");
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+
+    canon.setOrigin(0, 8);
+    canon.setPosition(gameSizeX / 2, groundH);
+    canon.setRotation(cannonRotation);
+    canon.setFillColor(sf::Color::Red);
+
+    ground.setPosition(0, groundH);
+    ground.setFillColor(sf::Color::Green);
+
+    auto plot = [](sf::Vector2f p) {
+        sf::RectangleShape p0(sf::Vector2f(16, 16));
+        p0.setOrigin(0, 8);
+        p0.setFillColor(sf::Color::Cyan);
+        p0.setOutlineColor(sf::Color::White);
+        p0.setOutlineThickness(2);
+        p0.setRotation(45);
+        p0.setPosition(p);
+        catmullPoints.push_back(p0);
+    };
+
+    plot(sf::Vector2f(100, 50));
+    plot(sf::Vector2f(50, 120));
+    plot(sf::Vector2f(200, 250));
+    plot(sf::Vector2f(20, 300));
+    plot(sf::Vector2f(200, 400));
+    plot(sf::Vector2f(100, 500));
+
+    plot(sf::Vector2f(0, 0));
+
+    sf::RectangleShape& tPoint = catmullPoints.back();
+    tPoint.setFillColor(sf::Color::Blue);
+
+    Line l;
+
+    std::vector<sf::Vector2f> p;
+    p.push_back(sf::Vector2f(100, 50));
+    p.push_back(sf::Vector2f(50, 120));
+    p.push_back(sf::Vector2f(200, 250));
+    p.push_back(sf::Vector2f(20, 300));
+    p.push_back(sf::Vector2f(200, 400));
+    p.push_back(sf::Vector2f(100, 500));
+    l.SetPoint(p);
+
+    while (window.isOpen()) //une frame
+    {
+        sf::Event event;
+        while (window.pollEvent(event)) //un event  il y a d'autre mode d'interraction que poll
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        /*
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            cannonRotation--;
+            canon.setRotation(cannonRotation);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            cannonRotation++;
+            canon.setRotation(cannonRotation);
+        }
+        */
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            catmullT -= 0.05f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
+            catmullT += 0.05f;
+        }
+
+        auto cannonPos = sf::Vertex(canon.getPosition());
+        cannonPos.color = sf::Color::Red;
+
+        sight.clear();
+        sight.append(cannonPos);
+
+        sf::Transform t;
+        auto tFinal = t.rotate(cannonRotation);
+        auto dest = tFinal.transformPoint(sf::Vector2f(128, 0));
+        auto destVertex = sf::Vertex(dest);
+        destVertex.position += cannonPos.position;
+        destVertex.color = sf::Color::Red;
+        sight.append(destVertex);
+
+        sf::Vector2f calc = CatMull::Polynom2(
+            catmullPoints[0].getPosition(),
+            catmullPoints[1].getPosition(),
+            catmullPoints[2].getPosition(),
+            catmullPoints[3].getPosition(), catmullT);
+        tPoint.setPosition(calc);
+
+        window.clear();
+        window.draw(ground);
+        window.draw(sight);
+        window.draw(canon);
+
+        for (auto& cp : catmullPoints)
+            window.draw(cp);
+
+        l.draw(window);
+
+        window.display();
+    }
+}
+
 int main()
 {
     //TestTuto();
-    TestLine();
+    //TestLine();
+    TestVertex();
 }

06f8c787e637e945474c22b562111cc2c9703c22
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2022-12-01 13:49:39 +0100

    push pif

---
+++ SFML Perso Cour 6/CommandeFile.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+#include "Comande.hpp"
+
+class CommandeFile {
+
+	static std::vector<Cmd> load(std::string path) {
+		//return nullptr si null
+		//sinon lis
+		//renvoie objet cmd file
+	}
+
+	static bool Save(std::string path, std::vector<Cmd>& data) {
+		//ouvrir en écriture
+		//
+	}
+};

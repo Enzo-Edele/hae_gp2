4c3ecb6ff5dd9898a75036a902ae0bf7e1d339db
Enzo-Edele <cloup.enzo@gmail.com>
2022-12-08 14:40:02 +0100

    prep cour 8 12/8

--- SFML Perso Cour 6/CommandeFile.hpp
+++ SFML Perso Cour 6/CommandeFile.hpp
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <vector>
-#include <sys/stat.h>
+#include<sys/stat.h>
 
 #include "Comande.hpp"
 
---
+++ SFML Perso Cour 8/Game.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+
+struct Game {
+public:
+	static const int gameX;
+	static const int gameY;
+
+	static const std::vector<Color> colors;
+
+	static const int cellSize;
+
+	static const float gravity;
+};
